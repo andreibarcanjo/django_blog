8a572fad2a29029372d2f0774eff6813f03d7715
Andrei b. Arcanjo <andreibarcanjo@gmail.com>
2025-08-25 11:00:04 -0300

    Feat: Add modelos dos posts, autor, data, referencia e anexo

--- blog/models.py
+++ blog/models.py
@@ -5,4 +5,10 @@ from django.db import models
 
 class Post(models.model):
     title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+    data_pub = models.DateTimeField(auto_now_add=True)
+    references = models.TextField(blank=True, null=True)
+    attachments = models.FileField(
+        upload_to='attachments/', blank=True, null=True)
     pass
